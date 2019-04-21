8c9ad5f5979d0099c2ca407790cbc7e3bf453fcd
Roman Syunyakov <romansunyakov@gmail.com>
2019-04-21 15:04:00 +0300

    Reflections done

--- 2 semester/Classwork/Lesson34/src/MainFramework.java
+++ 2 semester/Classwork/Lesson34/src/MainFramework.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class MainFramework {
     public static void main(String[] args) throws InstantiationException, IllegalAccessException, InvocationTargetException {
-        List<Student> lis1 = Framework.getMany(Student.class, 10, new Integer(10).intValue(), "Вася", new Integer(10).intValue());
+        List<Student> lis1 = Framework.getMany(Student.class, 10, 10, "Вася", 10);
         Arrays.stream(lis1.toArray()).forEach(s -> System.out.println(s));
         }
     }
